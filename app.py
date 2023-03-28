628492f00fc6d80cb0ea5b32a01a682eeaadd185
Gurvir Dua <gdua@terpmail.umd.edu>
2023-03-28 16:56:04 -0400

    First push of Module 8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,19 @@ def hello():  # put application's code here
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    fav_courses = [str(request.args.get('department') + request.args.get('course_no'))]
+    return render_template('favorite-course.html', courses=fav_courses)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
