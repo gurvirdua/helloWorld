cbe8e176b219497a68a9d2591816c4acd2152e10
Gurvir Dua <gdua@terpmail.umd.edu>
2023-04-04 19:19:03 -0400

    Adjusted app.py, third push of Module 9

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from models import db, Student, Major
 from datetime import datetime as dt
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'university.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'beyond_course_scope'
 db.init_app(app)
+
+
 
 
 @app.route('/student/view')
@@ -21,6 +25,8 @@ def student_view_all():
     return render_template('student_view_all.html', students=students)
 
 
+
+
 @app.route('/student/view/<int:student_id>')
 def student_view(student_id):
     student = Student.query.filter_by(student_id=student_id).first()
@@ -28,12 +34,15 @@ def student_view(student_id):
         .order_by(Major.major) \
         .all()
 
+
     if student:
         return render_template('student_entry.html', student=student, majors=majors, action='read')
+
 
     else:
         flash(f'Student attempting to be viewed could not be found!', 'error')
         return redirect(url_for('student_view_all'))
+
 
 @app.route('/student/create', methods=['GET', 'POST'])
 def student_create():
@@ -50,15 +59,19 @@ def student_create():
         email = request.form['email']
         is_honors = True if 'is_honors' in request.form else False
 
+
         student = Student(first_name=first_name, last_name=last_name, major_id=major_id,
-                          birth_date=dt.strptime(birth_date, '%Y-%m-%d'), email=email,  is_honors=is_honors)
+                          birth_date=dt.strptime(birth_date, '%Y-%m-%d'), email=email, is_honors=is_honors)
         db.session.add(student)
         db.session.commit()
         flash(f'{first_name} {last_name} was successfully added!', 'success')
         return redirect(url_for('student_view_all'))
 
+
     flash('Invalid action. Please try again.', 'error')
     return redirect(url_for('student_view_all'))
+
+
 
 
 @app.route('/student/update/<int:student_id>', methods=['GET', 'POST'])
@@ -69,14 +82,18 @@ def student_edit(student_id):
             .order_by(Major.major) \
             .all()
 
+
         if student:
             return render_template('student_entry.html', student=student, majors=majors, action='update')
+
 
         else:
             flash(f'Student attempting to be edited could not be found!', 'error')
 
+
     elif request.method == 'POST':
         student = Student.query.filter_by(student_id=student_id).first()
+
 
         if student:
             student.first_name = request.form['first_name']
@@ -88,18 +105,23 @@ def student_edit(student_id):
             student.gpa = request.form['gpa']
             student.is_honors = True if 'is_honors' in request.form else False
 
+
             db.session.commit()
             flash(f'{student.first_name} {student.last_name} was successfully updated!', 'success')
         else:
             flash(f'Student attempting to be edited could not be found!', 'error')
 
+
         return redirect(url_for('student_view_all'))
 
+
     return redirect(url_for('student_view_all'))
+
 
 @app.route('/student/delete/<int:student_id>')
 def student_delete(student_id):
     student = Student.query.filter_by(student_id=student_id).first()
+
 
     if student:
         db.session.delete(student)
@@ -108,7 +130,10 @@ def student_delete(student_id):
     else:
         flash(f'Delete failed! Student could not be found.', 'error')
 
+
     return redirect(url_for('student_view_all'))
+
+
 
 
 @app.route('/')
@@ -116,12 +141,15 @@ def home():
     return redirect(url_for('student_view_all'))
 
 
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
+
         # Execute only once! Initial loading of available majors
-"""
+        """ 
         majors = ['Accounting', 'Finance', 'Information Systems', 'International Business', 'Management', \
                     'Operations Management & Business Analytics', 'Supply Chain Management']
         for each_major in majors:
@@ -129,5 +157,7 @@ if __name__ == '__main__':
             amajor = Major(major=each_major)
             db.session.add(amajor)
             db.session.commit()
-"""
-app.run()
+        """
+    app.run()
+
+
